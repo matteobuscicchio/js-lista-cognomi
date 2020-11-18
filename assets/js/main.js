a1c4932f1e3c9dc52153f3d82ad85ece1e1bbe65
matteo <matteoezio@me.com>
2020-11-18 18:54:58 +0100

    stampato anche posizione utente

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,7 +17,13 @@ console.log(surnameArray.sort());
 
 // stampo i nomi nella lista ordinata
 for (var i=0; i<surnameArray.length; i++){
-    var olSurnames = surnameArray[i]
+
+    var olSurnames = surnameArray[i];
     var surnameOrderedList = document.getElementById('lista_cognomi').innerHTML;
     document.getElementById('lista_cognomi').innerHTML = surnameOrderedList + "<li>" + olSurnames + "</li>";
+    
 }
+
+// stampo posizione utente
+var stampUserPosition = surnameArray.indexOf(userSurname);
+console.log((stampUserPosition) + 1);
