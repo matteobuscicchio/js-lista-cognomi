322cc0a3c0368cca5292ff4790fa80491a8cb5bd
matteo <matteoezio@me.com>
2020-11-18 17:03:10 +0100

    ho impostato l'aray nel file .js

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+// lista cognomi
+var surnameArray = [
+    "Bianchi", 
+    "Rossi", 
+    "Duzioni", 
+    "Balsano", 
+    "Verdi"
+]
+console.log(surnameArray);
+
+// prompt di domanda
+var userSurname = prompt("Salve, qual'è il tuo cognome?");
+console.log(userSurname);
+
+// 
